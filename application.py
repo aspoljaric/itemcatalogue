ac880d53ccfe387f9e3b7f3b1713a577ba407627
Albert Spoljaric <albert.spoljaric@gmail.com>
2016-07-27 11:23:23 +1000

    feat: add templates
    add templates & mock data to test forms/templates

--- application.py
+++ application.py
@@ -1,52 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-# Catalogue operations
+# Mock Categories
+category = {'name': 'Soccer', 'id': '1'}
+categories = [{'name': 'Soccer', 'id': '1'}, {
+    'name': 'Tennis', 'id': '2'}, {'name': 'Football', 'id': '3'}]
+
+# Mock Items
+item = {'name': 'Ball', 'description': 'This is blue soccer ball.',
+        'picture': 'pic', 'id': '1'}
+items = [{'name': 'Ball', 'description': 'This is blue soccer ball.',
+          'picture': 'pic', 'id': '1'},
+         {'name': 'Tshirt', 'description': 'This is a tshirt.',
+             'picture': 'pic', 'id': '2'},
+         {'name': 'Shorts', 'description': 'Red shorts.',
+          'picture': 'pic', 'id': '3'}]
+
+# Category operations
 
 
 @app.route('/')
 @app.route('/catalogue')
 def showCatalogues():
-    return 'this page will show all my catalogues'
+    return render_template('categories.html', categories=categories)
 
 
 @app.route('/catalogue/new')
-def newCatalogue():
-    return 'this page will be for making a new catalogue'
+def newCategory():
+    return render_template('new-category.html')
 
 
-@app.route('/catalogue/<int:catalogue_id>/edit')
-def editCatalogue(catalogue_id):
-    return 'this page will be for editing catalogue %s' % catalogue_id
+@app.route('/catalogue/<int:category_id>/edit')
+def editCategory(category_id):
+    return render_template('edit-category.html', category=category)
 
 
-@app.route('/catalogue/<int:catalogue_id>/delete')
-def deleteCatalogue(catalogue_id):
-    return 'this page will be for deleting catalogue %s' % catalogue_id
-# Catalogue operations END
+@app.route('/catalogue/<int:category_id>/delete')
+def deleteCategory(category_id):
+    return render_template('delete-category.html', category=category)
+# Category operations END
 
 # Item operations
 
 
-@app.route('/catalogue/<int:catalogue_id>')
-@app.route('/catalogue/<int:catalogue_id>/items')
-def showItems(catalogue_id):
-    return 'this page will show the items for the catalogue %s' % catalogue_id
+@app.route('/catalogue/<int:category_id>')
+@app.route('/catalogue/<int:category_id>/items')
+def showItems(category_id):
+    return render_template('items.html', items=items)
 
 
-@app.route('/catalogue/<int:catalogue_id>/item/new')
-def newItem(catalogue_id):
-    return 'this page is for making a new item for catalogue %s' % catalogue_id
+@app.route('/catalogue/<int:category_id>/item/new')
+def newItem(category_id):
+    return render_template('new-item.html')
 
 
-@app.route('/catalogue/<int:catalogue_id>/item/<int:item_id>/edit')
-def editItem(catalogue_id, item_id):
-    return 'this page is for making a editing item %s' % item_id
+@app.route('/catalogue/<int:category_id>/item/<int:item_id>/edit')
+def editItem(category_id, item_id):
+    return render_template('edit-item.html', item=item)
 
 
-@app.route('/catalogue/<int:catalogue_id>/item/<int:item_id>/delete')
-def deleteItem(catalogue_id, item_id):
-    return 'this page is for making a deleting item %s' % item_id
+@app.route('/catalogue/<int:category_id>/item/<int:item_id>/delete')
+def deleteItem(category_id, item_id):
+    return render_template('delete-item.html', item=item)
 # Item operations END
 
 if __name__ == '__main__':
