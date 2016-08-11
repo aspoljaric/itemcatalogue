4d377d74459434b3aa9407af65e5282f71288fca
Albert Spoljaric <albert.spoljaric@gmail.com>
2016-08-11 14:06:39 +1000

    feat: add Google login

--- application.py
+++ application.py
@@ -1,41 +1,19 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify
+from db_connection import *
+from flask import Flask, render_template, url_for, request
+from flask import redirect, flash, jsonify
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 app = Flask(__name__)
 
-from database import Base, Category, Item
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
-from sqlalchemy_imageattach import store
 
-
-def connect():
-    """Connect to the SQLite database.  Returns a database session."""
-    try:
-        engine = create_engine('sqlite:///categories.db')
-        Base.metadata.bind = engine
-        DBSession = sessionmaker(bind=engine)
-        session = DBSession()
-        return session
-    except:
-        print("Connection failed.")
-
-
-@contextmanager
-def session_scope():
-    """Provide a transactional scope around a series of operations."""
-    session = connect()
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
-
-# Category operations
-
-
+# CRUD operations for catalogue
 @app.route('/')
 @app.route('/catalogue')
 def showCatagories():
@@ -77,9 +55,6 @@ def deleteCategory(category_id):
                 return redirect(url_for('showCatagories'))
         else:
             return render_template('delete-category.html', category=category)
-# Category operations END
-
-# Item operations
 
 
 @app.route('/catalogue/<int:category_id>')
@@ -135,10 +110,10 @@ def deleteItem(category_id, item_id):
         else:
             item = session.query(Item).filter_by(id=item_id).one()
             return render_template('delete-item.html', item=item)
-# Item operations END
+# END - CRUD operations for catalogue
 
 
-# JSON API Endpoints
+# JSON API endpoints
 @app.route('/catalogue/JSON')
 def catagoriesJSON():
     with session_scope() as session:
@@ -160,8 +135,167 @@ def itemInCategoryJSON(category_id, item_id):
         item = session.query(Item).filter_by(
             category_id=category_id).filter_by(id=item_id).one()
         return jsonify(Item=[item.serialize])
-# JSON API Endpoints END
+# END JSON API endpoints
+
+
+# User Authentication
+CLIENT_ID = json.loads(
+    open('client_secret_385075612489-vqap54od740ilifehpt6lokmbtcfe70d'
+         '.apps.googleusercontent.com.json',
+         'r').read())['web']['client_id']
+APPLICATION_NAME = "Item Catalogue App"
+
+# Create anti-forgery state token
+
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/googleConnect', methods=['POST'])
+def googleConnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets(
+            'client_secret_385075612489-vqap54od740ilifehpt6lokmbtcfe70d'
+            '.apps.googleusercontent.com.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+            json.dumps('Current user is already connected.'),
+            200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    login_session['credentials'] = credentials.access_token
+    login_session['provider'] = 'google'
+
+    # see if user exists, if it doesn't make a new one
+    #user_id = getUserID(data["email"])
+    # if not user_id:
+    #user_id = createUser(login_session)
+    #login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;'
+    '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    #flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+
+@app.route('/googleDisconnect')
+def googleDisconnect():
+    # Only disconnect a connected user.
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = credentials
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] != '200':
+        # For whatever reason, the given token was invalid.
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+# Disconnect based on provider
+
+
+@app.route('/signout')
+def signout():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            googleDisconnect()
+            del login_session['gplus_id']
+            del login_session['credentials']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        #del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showCatagories'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('showCatagories'))
+
+# END User Authentication
 
 if __name__ == '__main__':
+    app.secret_key = '17763f3d2dd9f6762cd967ee53efdb82'
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
---
+++ db_connection.py
@@ -0,0 +1,30 @@
+from database import Base, Category, Item
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+
+
+def connect():
+    """Connect to the SQLite database.  Returns a database session."""
+    try:
+        engine = create_engine('sqlite:///categories.db')
+        Base.metadata.bind = engine
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        return session
+    except:
+        print("Connection failed.")
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = connect()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
